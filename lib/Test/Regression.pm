73d09b80e02634db98d77901772fd7f8bf9261a1
Nicholas Bamber <nicholas@periapt.co.uk>
2009-09-20 10:51:36 +0100

    more CPAN housekeeping

--- lib/Test/Regression.pm
+++ lib/Test/Regression.pm
@@ -10,11 +10,11 @@ Test::Regression - Test library that can be run in two modes: once to generate o
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
